d5420446a4dbe32c70aa59cf4c24e3461d9067b6
Bruce Hem <bruce.hem@pathmazing.com>
2018-01-06 13:26:14 +0700

    add missing mailer export

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import 'reflect-metadata';
 export * from './auth';
 export * from './indicative';
 export * from './logger';
+export * from './mailer';
 export * from './redis';
 export * from './typeorm';
