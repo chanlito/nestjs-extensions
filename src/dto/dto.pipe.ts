c25fdb828e59bc0aea71dd62de39e0d5b26e808e
Chanlito <chanlito@outlook.com>
2018-02-06 16:47:57 +0700

    fix error handler

--- src/dto/dto.pipe.ts
+++ src/dto/dto.pipe.ts
@@ -2,7 +2,7 @@ import { UnprocessableEntityException } from '@nestjs/common';
 import { ArgumentMetadata, PipeTransform } from '@nestjs/common/interfaces';
 import { Reflector } from '@nestjs/core';
 import { plainToClass } from 'class-transformer';
-import { validate, ValidationError } from 'class-validator';
+import { validate } from 'class-validator';
 
 import { DTO_META_KEY } from './dto.constants';
 import { DtoOptions } from './dto.interfaces';
@@ -19,22 +19,11 @@ export class DtoPipe implements PipeTransform<any> {
       if (errors.length > 0) {
         throw new UnprocessableEntityException({
           message: 'Validation Failed.',
-          errors: errors.map(x => this.mapError(x))
+          errors
         });
       }
       return entity;
     }
     return value;
-  }
-
-  private mapError(err: ValidationError) {
-    console.log('map error', err);
-    if (err.constraints) {
-      return {
-        field: err.property,
-        validation: Object.keys(err.constraints)[0],
-        message: err.constraints[Object.keys(err.constraints)[0]]
-      };
-    } else return this.mapError(err.children[0]);
   }
 }
