50c4d864bad6e60386ad8f08e60379e767034a5d
Chanlito <chanlito@outlook.com>
2018-02-06 17:31:10 +0700

    update error

--- src/dto/dto.pipe.ts
+++ src/dto/dto.pipe.ts
@@ -2,7 +2,7 @@ import { UnprocessableEntityException } from '@nestjs/common';
 import { ArgumentMetadata, PipeTransform } from '@nestjs/common/interfaces';
 import { Reflector } from '@nestjs/core';
 import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
+import { validate, ValidationError } from 'class-validator';
 
 import { DTO_META_KEY } from './dto.constants';
 import { DtoOptions } from './dto.interfaces';
@@ -19,11 +19,22 @@ export class DtoPipe implements PipeTransform<any> {
       if (errors.length > 0) {
         throw new UnprocessableEntityException({
           message: 'Validation Failed.',
-          errors
+          errors: errors.map(x => this.mapError(x, ''))
         });
       }
       return entity;
     }
     return value;
+  }
+
+  private mapError(err: ValidationError, name: string) {
+    const field = `${name}${name ? '.' : ''}${err.property}`;
+    if (err.constraints) {
+      return {
+        field,
+        validation: Object.keys(err.constraints)[0],
+        message: err.constraints[Object.keys(err.constraints)[0]]
+      };
+    } else return this.mapError(err.children[0], field);
   }
 }
