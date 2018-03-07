1c2e9761706e13e0a0f63fb1c4e19ac2e47b52ea
Chanlito <chanlito@outlook.com>
2018-03-07 21:02:33 +0700

    fix exception filter

--- src/exception/app-exception.filter.ts
+++ src/exception/app-exception.filter.ts
@@ -2,38 +2,33 @@ import { Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 @Catch()
 export class ApplicationExceptionFilter implements ExceptionFilter {
   catch(err: any, res: any) {
-    const errTitle = `\n ¯\\_(ツ)_/¯ Meh, it's just an error! Don't worry about it. \n`;
+    const statusCode = typeof err.getStatus === 'function' ? err.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const response = typeof err.getResponse === 'function' ? err.getResponse() : 'Internal Server Error';
 
-    if (err instanceof Error) {
-      console.error(errTitle, err);
-      const statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
-      res.status(statusCode).json({
+    let responseBody: any;
+
+    if (typeof response === 'string') {
+      responseBody = {
         statusCode,
-        message: 'Internal Server Error'
-      });
+        message: response
+      };
+    } else if (typeof response.message === 'string') {
+      responseBody = {
+        statusCode,
+        message: response.message
+      };
     } else {
-      const statusCode = err.getStatus();
-      const response = err.getResponse();
-
-      if (typeof response === 'string') {
-        console.error(errTitle, response);
-        res.status(statusCode).json({
-          statusCode,
-          message: response
-        });
-      } else if (typeof response.message === 'string') {
-        console.error(errTitle, response.message);
-        res.status(statusCode).json({
-          statusCode,
-          message: response.message
-        });
-      } else {
-        console.error(errTitle, response.message);
-        res.status(statusCode).json({
-          statusCode,
-          ...response.message
-        });
-      }
+      responseBody = {
+        statusCode,
+        ...response.message
+      };
     }
+
+    if (process.env.NODE_ENV !== 'production') {
+      responseBody = { ...responseBody, stack: err.stack };
+    }
+
+    console.error(responseBody);
+    res.status(statusCode).json(responseBody);
   }
 }
