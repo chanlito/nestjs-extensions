072a3fe7bb51431d786e240b23e5b565b5231cdf
Chanlito <chanlito@outlook.com>
2018-03-11 00:12:50 +0700

    log app exception filter

--- src/exception/app-exception.filter.ts
+++ src/exception/app-exception.filter.ts
@@ -1,7 +1,11 @@
 import { Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
 
+import { Logger } from '../logger';
+
 @Catch()
 export class ApplicationExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger?: Logger) {}
+
   catch(err: any, res: any) {
     const statusCode = typeof err.getStatus === 'function' ? err.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
     const response = typeof err.getResponse === 'function' ? err.getResponse() : 'Internal Server Error';
@@ -26,5 +30,13 @@ export class ApplicationExceptionFilter implements ExceptionFilter {
     }
 
     res.status(statusCode).json(responseBody);
+
+    if (this.logger) {
+      if (statusCode >= 500) {
+        this.logger.error(responseBody, responseBody.message);
+      } else {
+        this.logger.warn(responseBody, responseBody.message);
+      }
+    }
   }
 }
