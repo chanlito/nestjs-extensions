56488b4502a3faa3b241dd3ae0ee6a772323233a
Chanlito <chanlito@outlook.com>
2018-03-15 09:57:28 +0700

    fix mongodb issue

--- src/typeorm/typeorm.providers.ts
+++ src/typeorm/typeorm.providers.ts
@@ -25,7 +25,7 @@ export function createTypeOrmProviders(config: CreateTypeOrmProvidersConfigurati
     },
     {
       provide: MongoEntityManager,
-      useFactory: (connection: Connection) => connection.mongoManager,
+      useFactory: (connection: Connection) => (connection.options.type === 'mongodb' ? connection.mongoManager : null),
       inject: [Connection]
     }
   ];
